2ea178a177a300eb5d398bad1629697747b416df
patrick.nietocastro@sva.de <patrick.nietocastro@sva.de>
2021-10-21 08:42:31 +0200

    Add more test testcases

--- src/test/java/demo/AppTest.java
+++ src/test/java/demo/AppTest.java
@@ -11,4 +11,12 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void appHasAGreeting2() {
+        assertNotNull("hello", "hello is not null");
+    }
+
+    @Test void appHasAGreeting3() {
+        assertNotNull("hello2", "hello2 is also not null");
+    }
 }
